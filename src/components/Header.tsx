84c2d32725698e37bfd2f4404471e20207848626
Filipe Rodrigues <eulerfilipe8@outlook.com>
2023-06-19 15:16:19 -0300

    feat: Creating Routes and Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+// import EffectLeft from '../assets/effects/left.svg'
+// import EffectRight from '../assets/effects/right.svg'
+import Logo from '../assets/logo.svg'
+
+export function Header() {
+  return (
+    <header className="h-[296px] w-full bg-base-profile flex items-center justify-center">
+      <img src={Logo} alt="" />
+    </header>
+  )
+}
