26c627d39df5e9fa5654d6fda39e933b6987e71c
Filipe Rodrigues <eulerfilipe8@outlook.com>
2023-06-20 14:19:02 -0300

    feat: standardization of routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import { Blog } from './pages/Blog'
+import { Post } from './pages/Post'
+import { Default } from './defaultLayout/default'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Blog />} />
+      <Route path="/" element={<Default />}>
+        <Route path="/" element={<Blog />} />
+        <Route path="/post" element={<Post />} />
+      </Route>
     </Routes>
   )
 }
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,16 +1,11 @@
-import { Header } from '../../components/Header'
 import { Profile } from './components/Profile'
 import { SearchForm } from './components/SearchForm'
 
 export function Blog() {
   return (
-    <div>
-      <Header />
-
-      <main className="max-w-4xl mx-auto px-5">
-        <Profile />
-        <SearchForm />
-      </main>
+    <div className="max-w-4xl mx-auto px-5">
+      <Profile />
+      <SearchForm />
     </div>
   )
 }
