ae17c9fbac5e83338f882235d03f8c2ccbd32c20
Filipe Rodrigues <eulerfilipe8@outlook.com>
2023-06-22 18:07:12 -0300

    fix: refactor

--- src/pages/Blog/components/Profile.tsx
+++ src/pages/Blog/components/Profile.tsx
@@ -34,15 +34,15 @@ export function Profile() {
   return (
     <div
       style={{ boxShadow: '0px 2px 28px rgba(0, 0, 0, 0.2)' }}
-      className="bg-base-profile flex items-center rounded-[10px] gap-8 h-[212px] px-10 py-8 -mt-20"
+      className="bg-base-profile flex items-center rounded-[10px] gap-8 h-[212px] px-10 py-8 -mt-20 max-md:h-full max-sm:flex-col"
     >
       <img
         src={userGithubProfile.avatar_url}
         alt=""
         draggable={false}
-        className="rounded-lg w-36 h-36 select-none"
+        className="rounded-lg w-36 h-36 select-none max-md:self-start max-sm:self-center"
       />
-      <div className="flex flex-col gap-2 flex-1">
+      <div className="flex flex-col gap-2 flex-1 w-full">
         <div className="flex justify-between">
           <h1 className="text-base-title text-2xl font-bold">
             {userGithubProfile.name}
@@ -63,7 +63,7 @@ export function Profile() {
             {userGithubProfile.bio}
           </p>
 
-          <div className="flex items-center gap-6">
+          <div className="flex items-center gap-6 max-md:flex-col max-md:items-start">
             <div className="flex items-center gap-2">
               <GithubLogo weight="fill" color="#3A536B" />
               <span className="text-base-subtitle text-base font-normal">
